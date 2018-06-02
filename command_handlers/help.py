85fc47abf1f3b413c04ce5b0a42157de37c03803
Mattia Natali <matitalatina@gmail.com>
2018-06-02 18:58:33 +0200

    Add parse mode Markdown

--- command_handlers/help.py
+++ command_handlers/help.py
@@ -10,12 +10,12 @@ class HelpCommandHandler(CommandHandler):
     def callback(bot, update):
         bot.send_message(update.message.chat_id,
                          text=f"""
-• **Oracolo**: concludi la frase con "?".\n
-  Esempio: *Oggi dovrei studiare?*\n
-• **Lista**: proponimi qualche alternativa separando le scelte con la virgola. Io ti mostrerò la scelta migliore.\n
+• *Oracolo*: concludi la frase con "?".\n
+  Esempio: _Oggi dovrei studiare?_\n
+• *Lista*: proponimi qualche alternativa separando le scelte con la virgola. Io ti mostrerò la scelta migliore.\n
   Esempio: cinema, bowling, bar\n
-• **Numeri**: "*Scegli un numero tra 0 e 25*", "*Scegli 6 numeri senza ripetizioni da 1 a 90*", "*Scegli una dozzina di numeri*", "*Scegli un numero fino a 10*"\n
-• **Colore**: "*Scegli un colore*"\n
+• *Numeri*: "_Scegli un numero tra 0 e 25_", "_Scegli 6 numeri senza ripetizioni da 1 a 90_", "_Scegli una dozzina di numeri_", "_Scegli un numero fino a 10_"\n
+• *Colore*: "_Scegli un colore_"\n
  
 Nei gruppi ricordati di chiamarmi scrivendo /rm@{bot.username} all'inizio del messaggio.""",
                          parse_mode=ParseMode.MARKDOWN)
